3d11cd46bed3805c0804ac6fd441cabeaf578ff0
jrwallace1983 <jrwallace1983@gmail.com>
2020-12-07 22:59:00 -0800

    Update convertTranscript.js

--- app/js/convertTranscript.js
+++ app/js/convertTranscript.js
@@ -1,5 +1,24 @@
 const lineReader = require('line-reader');
+let lines = []
 
 lineReader.eachLine('../python/download.txt', (line,last)=>{
-    console.log(line)
+    if(line.includes('<p')){
+        lines.push(line)
+    }
+    
+}, ()=>{
+    for(let i = 0; i<lines.length; i++){
+    let lineList = lines[i].split(' ');
+    for(let a = 0; a<lineList.length; a++){
+    if(lineList[a].includes('begin=')){
+        console.log(lineList[a])
+        let beginSeconds = lineList[a].replace(/[begin=t"]/g,'')
+        console.log(beginSeconds)
+    }else if(lineList[a].includes('end=')){
+        console.log(lineList[a])
+        let endSeconds = lineList[a].replace(/[end=t"]/g,'')
+        console.log(endSeconds)
+    }
+        }
+    }
 })
